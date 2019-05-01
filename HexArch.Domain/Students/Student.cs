b2b25ef37ec3fdab9f628460551a39b7bc5eb8a4
Maciej Borowy <noobchar12345@gmail.com>
2019-05-01 20:50:46 +0200

    Initial project structure

---
+++ HexArch.Domain/Students/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HexArch.Domain.Students
+{
+    public class Student
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
---
+++ HexArch.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HexArch.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
